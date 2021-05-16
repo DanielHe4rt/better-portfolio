34ae7dcef9e07f9b3110accd503eb31f936918ee
danielhe4rt <idanielreiss@gmail.com>
2021-05-16 15:20:58 -0300

    agora vai

--- resources/views/support.blade.php
+++ resources/views/support.blade.php
@@ -2,7 +2,12 @@
 
 @section('content')
     <div class="col-xs-12 col-md-9" style="border-left: 1px solid rgba(194,194,194,0.1);">
-        <section class="why-support">
+        <section class="links">
+            <a href="#why">Por quê apoiar </a>
+            <a href="#projects">Meus Projetos</a>
+            <a href="#how-support">Como apoiar</a>
+        </section>
+        <section class="why-support" id="why">
             <h3 class="title">Por quê você deveria me apoiar?</h3>
             <p>
                 Crio conteúdo na <a href="https://twitch.tv/danielhe4rt" target="_blank">Twitch.tv</a> há quase 3 anos
@@ -19,72 +24,70 @@
             </p>
         </section>
 
-        <section class="my-projects">
+        <section class="my-projects" id="projects">
             <h3 class="title">Quais projetos eu fiz/faço</h3>
             <p>Se você quiser entender mais sobre minhas contribuições mais recentes à comunidade dev, se liga nos links abaixo:</p>
             <div class="projects">
-                <a href="#" class="project">
-                    <img src="https://placehold.it/300x200" alt="">
+                <a href="https://github.com/danielhe4rt/php4noobs" class="project">
+                    <img src="{{ asset('img/git-php4noobs.png') }}" alt="">
                     <div class="project-title">PHP4Noobs - Github</div>
                     <p> Tutorial De PHP para iniciantes na linguagem.</p>
                 </a>
-                <a href="#" class="project">
-                    <img src="https://placehold.it/300x200" alt="">
+                <a href="https://github.com/danielhe4rt/git4noobs" class="project">
+                    <img src="{{ asset('img/git-git4noobs.png') }}" alt="">
                     <div class="project-title">Git4Noobs - Github</div>
                     <p> Tutorial De Git para iniciantes com a ferramenta.</p>
                 </a>
-                <a href="#" class="project">
-                    <img src="https://placehold.it/300x200" alt="">
+                <a href="https://udemy.com/php4noobs" class="project">
+                    <img src="{{ asset('img/udemy-php4noobs.png') }}" alt="">
                     <div class="project-title">PHP4Noobs - Udemy</div>
                     <p> Curso gratuito para iniciar novos programadores na linguagem PHP.</p>
                 </a>
-                <a href="#" class="project">
-                    <img src="https://placehold.it/300x200" alt="">
+                <a href="https://udemy.com/laravel4noobs" class="project">
+                    <img src="{{ asset('img/udemy-laravel4noobs.png') }}" alt="">
                     <div class="project-title">Laravel4Noobs - Udemy</div>
                     <p> Curso gratuito para iniciar novos programadores na Framework Laravel.</p>
                 </a>
             </div>
         </section>
 
-        <section class="how-support">
+        <section class="how-support" id="how-support">
             <h3 class="title">Como você consegue me apoiar?</h3>
             <!-- Collapse -->
-
-
             <div class="accordion" id="accordionExample">
                 <div class="method">
                     <h2 class="mb-0">
-                        <button class="btn btn-block text-left btn-no-radius" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                        <button class="btn btn-link btn-block text-left btn-no-radius" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
                             Amazon Prime (Twitch Prime)
                         </button>
                     </h2>
-                    <div id="collapseOne" class="collapse active show" aria-labelledby="headingOne" data-parent="#accordionExample">
+                    <div id="collapseOne" class="collapse " aria-labelledby="headingOne" data-parent="#accordionExample">
                         <div class="method-body">
 
                             <p>Esse modelo de apoio custa ZERO para você que tenha Amazon Prime e não utiliza-o na Twitch.</p>
 
                             <h4 class="title">1. Crie uma conta na Twitch</h4>
-                            <img class="img-thumbnail"src="https://i.imgur.com/PH8a85z.png" alt="">
+                            <img class="img-thumbnail" src="https://i.imgur.com/PH8a85z.png" alt="">
                             <p>Crie uma conta na Twitch TV para que você possa seguir meu canal.</p>
                             <div class="block"></div>
                             <h4 class="title">2. Procure o botão de vinculação do Amazon Prime</h4>
-                            <img class="img-thumbnail"src="https://i.imgur.com/boW0JXs.png" alt="">
+                            <img class="img-thumbnail" src="https://i.imgur.com/boW0JXs.png" alt="">
                             <p>
                                 Clique na coroa ao lado do seu usuário, localizado ao lado direito da barra de navegação
                                 e clique para Iniciar o Prime Gaming.
                             </p>
                             <div class="block"></div>
                             <h4 class="title">3. Vincule seu Amazon Prime</h4>
-                            <img class="img-thumbnail"src="https://i.imgur.com/c8u2MQy.png" alt="">
+                            <img class="img-thumbnail" src="https://i.imgur.com/c8u2MQy.png" alt="">
                             <p>
                                 Clique no botão "Try Prime" ou algo semelhante nessa tela caso seu sistema seja em português.<br>
                                 Siga todos os passos para que o Prime Gaming seja vinculado à sua conta da Twitch.
                             </p>
                             <div class="block"></div>
                             <h4 class="title">4. Entre no meu canal da Twitch e clique no Inscrever</h4>
-                            <img class="img-thumbnail"src="https://i.imgur.com/yL4kl2B.png" alt="">
+                            <img class="img-thumbnail" src="https://i.imgur.com/yL4kl2B.png" alt="">
                             <p>ou</p>
-                            <img class="img-thumbnail"src="https://i.imgur.com/1modPgt.png" alt="">
+                            <img class="img-thumbnail" src="https://i.imgur.com/1modPgt.png" alt="">
                             <p>
                                 Ao entrar no meu canal, se eu estiver offline, clique na aba Chat ou procure pelo botão
                                 Subscribe ou Inscrever-se.
